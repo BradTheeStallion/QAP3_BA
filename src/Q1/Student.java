ed4be3eaa6e65cd0ec9329389bdc7e61d08fa20d
BradTheeStallion <bradley.ayers@keyin.com>
2024-11-20 19:55:19 -0330

    Initial Commit and Setup of Directory Structure and beginning of first problem.

---
+++ src/Q1/Student.java
@@ -0,0 +1,38 @@
+package Q1;
+
+class Student extends Person {
+    protected String myIdNum; // Student Id Number
+    protected double myGPA; // grade point average
+
+    public Student(String name, int age, String gender, String idNum, double gpa) {
+        // use the super class’ constructor
+        super(name, age, gender);
+
+        // initialize what’s new to Student
+
+        myIdNum = idNum;
+
+        myGPA = gpa;
+
+    } 
+
+    public String toString() {
+        return super.toString() + ", student id: " + myIdNum + ", gpa: " + myGPA;
+    }
+
+    public String getMyIdNum() {
+        return this.myIdNum;
+    }
+
+    public void setMyIdNum(String myIdNum) {
+        this.myIdNum = myIdNum;
+    }
+
+    public double getMyGPA() {
+        return this.myGPA;
+    }
+
+    public void setMyGPA(double myGPA) {
+        this.myGPA = myGPA;
+    }
+}

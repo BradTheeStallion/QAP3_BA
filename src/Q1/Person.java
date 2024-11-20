03651d468cfc4f8455aac2364c2103d612f796a2
BradTheeStallion <bradley.ayers@keyin.com>
2024-11-20 20:01:43 -0330

    Added getters and setters

--- src/Q1/Person.java
+++ src/Q1/Person.java
@@ -1,4 +1,38 @@
 package Q1;
 
-public class Person {
+class Person {
+
+    protected String myName ; // name of the person
+    protected int myAge; // person’s age
+    protected String myGender; // “M” for male, “F” for female
+
+    public Person(String name, int age, String gender) {
+
+        myName = name; myAge = age ; myGender = gender; }
+
+    public String toString() { return myName + ", age: " + myAge + ", gender: " + myGender; }
+
+    public String getMyName() {
+        return this.myName;
+    }
+
+    public void setMyName(String myName) {
+        this.myName = myName;
+    }
+
+    public int getMyAge() {
+        return this.myAge;
+    }
+
+    public void setMyAge(int myAge) {
+        this.myAge = myAge;
+    }
+
+    public String getMyGender() {
+        return this.myGender;
+    }
+
+    public void setMyGender(String myGender) {
+        this.myGender = myGender;
+    }
 }

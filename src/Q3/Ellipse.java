870e3a8af898d372c609821befd2d2a11649921d
BradTheeStallion <bradley.ayers@keyin.com>
2024-11-21 10:37:29 -0330

    Created Ellipse class

---
+++ src/Q3/Ellipse.java
@@ -0,0 +1,23 @@
+package Q3;
+
+public class Ellipse extends Shape {
+    private double a; // Major axis
+    private double b; // Minor axis
+    
+    public Ellipse(double axis1, double axis2) {
+        super("Ellipse");
+        this.a = Math.max(axis1, axis2);
+        this.b = Math.min(axis1, axis2);
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * a * b;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return Math.PI * 2 * Math.sqrt((a * a + b * b) / 2.0);
+    }
+}
+

2c8167e0aca96425e6da1f74080b922a5603cc00
BradTheeStallion <bradley.ayers@keyin.com>
2024-11-21 07:08:59 -0330

    MovablePoint class

---
+++ src/Q2/MovablePoint.java
@@ -0,0 +1,45 @@
+package Q2;
+
+public class MovablePoint extends Point {
+    private float xSpeed;
+    private float ySpeed;
+
+    public MovablePoint() {
+        super();
+        this.xSpeed = 0.0f;
+        this.ySpeed = 0.0f;
+    }
+
+    public MovablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x, y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public float getXSpeed() {
+        return this.xSpeed;
+    }
+
+    public float getYSpeed() {
+        return this.ySpeed;
+    }
+
+    public void setXSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public void setYSpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    public void move() {
+        setX(getX() + getXSpeed());
+        setY(getY() + getYSpeed());
+    }
+    
+    @Override
+    public String toString() {
+        return super.toString() + ", speed=(" + xSpeed + ", " + ySpeed + ")";
+    }
+}
+
